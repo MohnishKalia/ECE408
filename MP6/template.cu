cf6bf0849ba5015790c83d1ab010296fb6f8db2e
Mohnish Kalia <mohnish.kalia@gmail.com>
2023-10-25 00:48:29 -0500

    MP6 Correct Sol, cleanup needed

--- MP6/template.cu
+++ MP6/template.cu
@@ -18,11 +18,59 @@
     }                                                                     \
   } while (0)
 
+__global__ void finish_scan(float *input, float *output, int len, float* blockSumInput, int blockSumInputLen) {
+  int bx = blockIdx.x;
+  int tx = threadIdx.x;
+  int i = 2 * BLOCK_SIZE * bx + tx;
+
+  int blockSum = 0;
+  if (bx-1 >= 0)
+    blockSum = blockSumInput[bx-1];
+  if (i < len)
+    output[i] = input[i] + blockSum;
+}
+
 __global__ void scan(float *input, float *output, int len) {
   //@@ Modify the body of this function to complete the functionality of
   //@@ the scan on the device
   //@@ You may need multiple kernel calls; write your kernels before this
   //@@ function and call them from the host
+  __shared__ float T[2*BLOCK_SIZE];
+
+  int i = 2 * BLOCK_SIZE * blockIdx.x + threadIdx.x;
+  if (i < len) 
+    T[threadIdx.x] = input[i];
+  else
+    T[threadIdx.x] = 0;
+  
+  // printf("tidx: %d = %f\n", threadIdx.x, T[threadIdx.x]);
+  // T[threadIdx.x] = i < len ? input[i] : 0.0f;
+
+  // scan step
+  int stride = 1;
+  while(stride < 2*BLOCK_SIZE) {
+    __syncthreads();
+    int index = (threadIdx.x+1)*stride*2 - 1;
+    if(index < 2*BLOCK_SIZE && (index-stride) >= 0)
+      T[index] += T[index-stride];
+    // todo: need the syncthreads here?
+    stride = stride*2;
+  }
+  __syncthreads();
+
+  // post scan step
+  stride = BLOCK_SIZE/2;
+  while(stride > 0) {
+    __syncthreads();
+    int index = (threadIdx.x+1)*stride*2 - 1;
+    if ((index+stride) < 2*BLOCK_SIZE)
+      T[index+stride] += T[index];
+    stride = stride / 2;
+  }
+  __syncthreads();
+
+  if (i < len) 
+    output[i] = T[threadIdx.x];
 }
 
 int main(int argc, char **argv) {
@@ -58,12 +106,75 @@ int main(int argc, char **argv) {
   wbTime_stop(GPU, "Copying input memory to the GPU.");
 
   //@@ Initialize the grid and block dimensions here
+  dim3 DimBlock(2*BLOCK_SIZE, 1, 1);
+  int numOfBlockSums = ceil(numElements * (1.0f) / DimBlock.x);
+  dim3 DimGrid(numOfBlockSums, 1, 1);
+  wbLog(TRACE, "The number of block sums is ", DimGrid.x, ", block size is ", DimBlock.x);
 
   wbTime_start(Compute, "Performing CUDA computation");
   //@@ Modify this to complete the functionality of the scan
   //@@ on the deivce
 
+  // initial scan per block
+  scan<<<DimGrid, DimBlock>>>(deviceInput, deviceOutput, numElements);
   cudaDeviceSynchronize();
+  wbCheck(cudaMemcpy(hostOutput, deviceOutput, numElements * sizeof(float),
+                     cudaMemcpyDeviceToHost));
+  // for (int i = 0; i < 12; i++) {
+  //   wbLog(TRACE, "hI[", i, "] = ", hostInput[i], "; ", "hO[", i, "] = ", hostOutput[i]);
+  // }
+
+  // setup block sum scan
+  float *h_blockSumInput = (float *)malloc(numOfBlockSums * sizeof(float));
+  float *h_blockSumOutput = (float *)malloc(numOfBlockSums * sizeof(float));
+
+  for (int i = 0; i < numOfBlockSums; i++) {
+    int targetIdx = (i+1) * DimBlock.x - 1;
+    if (targetIdx > numElements-1) 
+      targetIdx = numElements-1;
+    h_blockSumInput[i] = hostOutput[targetIdx];
+    wbLog(TRACE, "BSI[", i, "] = ", h_blockSumInput[i], " //", targetIdx);
+  }
+
+  float *d_blockSumInput;
+  float *d_blockSumOutput;
+  wbCheck(cudaMalloc((void **)&d_blockSumInput, numOfBlockSums * sizeof(float)));
+  wbCheck(cudaMalloc((void **)&d_blockSumOutput, numOfBlockSums * sizeof(float)));
+  wbCheck(cudaMemcpy(d_blockSumInput, h_blockSumInput, numOfBlockSums * sizeof(float),
+                     cudaMemcpyHostToDevice));
+
+  // scan over block sums
+  dim3 BSDimGrid(1, 1, 1);
+  scan<<<BSDimGrid, DimBlock>>>(d_blockSumInput, d_blockSumOutput, numOfBlockSums);
+  cudaDeviceSynchronize();
+  // copy, free device memory
+  wbCheck(cudaMemcpy(h_blockSumOutput, d_blockSumOutput, numOfBlockSums * sizeof(float),
+                     cudaMemcpyDeviceToHost));
+  cudaFree(d_blockSumInput);
+  cudaFree(d_blockSumOutput);
+  cudaFree(deviceInput);
+  cudaFree(deviceOutput);
+
+  for (int i = 0; i < numOfBlockSums; i++) {
+    wbLog(TRACE, "BSO[", i, "] = ", h_blockSumOutput[i]);
+  }
+
+  // post scan procedure
+  // TODO: Maybe check for correctness with references
+  wbCheck(cudaMalloc((void **)&deviceInput, numElements * sizeof(float)));
+  wbCheck(cudaMalloc((void **)&deviceOutput, numElements * sizeof(float)));
+  wbCheck(cudaMemset(deviceOutput, 0, numElements * sizeof(float)));
+  wbCheck(cudaMemcpy(deviceInput, hostOutput, numElements * sizeof(float),
+                     cudaMemcpyHostToDevice));
+  wbCheck(cudaMalloc((void **)&d_blockSumInput, numOfBlockSums * sizeof(float)));
+  wbCheck(cudaMemcpy(d_blockSumInput, h_blockSumOutput, numOfBlockSums * sizeof(float),
+                    cudaMemcpyHostToDevice));
+  finish_scan<<<DimGrid, DimBlock>>>(deviceInput, deviceOutput, numElements, d_blockSumInput, numOfBlockSums);
+  
+  // free referenced block sum memory
+  free(h_blockSumInput);
+  free(h_blockSumOutput);
+
   wbTime_stop(Compute, "Performing CUDA computation");
 
   wbTime_start(Copy, "Copying output memory to the CPU");
