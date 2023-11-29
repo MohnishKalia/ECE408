6fda932a6992a4f113a338de29eae3b40fac8311
Mohnish Kalia <mohnish.kalia@gmail.com>
2023-11-29 00:23:00 -0600

    PM3 swap over forward, report somewhat complete

--- Project/custom/new-forward.cu
+++ Project/custom/new-forward.cu
@@ -1,11 +1,13 @@
 #include <cmath>
 #include <iostream>
 #include "gpu-new-forward.h"
+#include "cuda_fp16.h"
 
-#define TILE_WIDTH 16
-#define NUM_STREAMS 5
+// current optime sum: 54.01
 
-__global__ void conv_forward_kernel(const int streamNum, float * __restrict__ output, const float * __restrict__ input, const float * __restrict__ mask, const int B, const int M, const int C, const int H, const int W, const int K,const int S)
+#define TILE_WIDTH 17
+
+__global__ void conv_forward_kernel(__half * __restrict__ output, const __half * __restrict__ input, const __half * __restrict__ mask, const int B, const int M, const int C, const int H, const int W, const int K,const int S)
 {
     /*
     Modify this function to implement the forward pass described in Chapter 16.
@@ -40,9 +42,9 @@ __global__ void conv_forward_kernel(const int streamNum, float * __restrict__ ou
     // Insert your GPU convolution kernel code here
 
     // same as grid setup
-    int W_size = ceil(1.0f*W_out/TILE_WIDTH); // number of horizontal tiles per output map
-    int H_size = ceil(1.0f*H_out/TILE_WIDTH); // number of vertical tiles per output map
-    int b = min((int) blockIdx.z + (ceil(1.0f * B / NUM_STREAMS) * streamNum), B); // batch num is based on streamNum iteration
+    const int W_size = ceil(1.0f*W_out/TILE_WIDTH); // number of horizontal tiles per output map
+    const int H_size = ceil(1.0f*H_out/TILE_WIDTH); // number of vertical tiles per output map
+    int b = blockIdx.z;
     int m = blockIdx.x;
     int h = (blockIdx.y / W_size) * TILE_WIDTH + threadIdx.y; // target h of output
     int w = (blockIdx.y % W_size) * TILE_WIDTH + threadIdx.x; // target w of output
@@ -51,24 +53,22 @@ __global__ void conv_forward_kernel(const int streamNum, float * __restrict__ ou
     if (w < 0 || w >= W_out || h < 0 || h >= H_out)
         return;
 
-    float acc = 0.0f;
+    __half acc = 0;
     for (int c = 0; c < C; c++) { // sum over all input channels
         for (int p = 0; p < K; p++) { // loop over KxK filter
             for (int q = 0; q < K; q++) {
                 int h_idx = (h * S + p);
                 int w_idx = (w * S + q);
-                float val = 0.0f;
                 // if target idx is not within input bounds, use 0, otherwise grab value
-                if (!(w_idx < 0 || w_idx >= W || h_idx < 0 || h_idx >= H))
-                    val = in_4d(b, c, h_idx, w_idx);
-                acc += val * mask_4d(m, c, p, q);
+                if (!(w_idx < 0 || w_idx >= W || h_idx < 0 || h_idx >= H)) {
+                    acc = __hfma(in_4d(b, c, h_idx, w_idx), mask_4d(m, c, p, q), acc);
+                }
             }
         }
     }
 
     // after accumulating, set to output value
     out_4d(b, m, h, w) = acc;
-    // atomicAdd(&(out_4d(b, m, h, w)), acc);
 
     #undef out_4d
     #undef in_4d
@@ -104,50 +104,35 @@ __host__ void GPUInterface::conv_forward_gpu_prolog(const float *host_output, co
     const int H_out = (H - K)/S + 1;
     const int W_out = (W - K)/S + 1;
 
-    size_t dop_sz = B * M * H_out * W_out * sizeof(float);
-    size_t dip_sz = B * C * H * W * sizeof(float);
-    size_t dmp_sz = M * C * K * K * sizeof(float);
+    size_t dop_sz = B * M * H_out * W_out * sizeof(__half);
+    size_t dip_sz = B * C * H * W * sizeof(__half);
+    size_t dmp_sz = M * C * K * K * sizeof(__half);
+
+    // __half* fp_16_output_ptr = (__half*) malloc(dop_sz);
+    __half* fp_16_input_ptr = (__half*) malloc(dip_sz);
+    __half* fp_16_mask_ptr = (__half*) malloc(dmp_sz);
+
+    for (size_t i = 0; i < B * C * H * W; i++)
+    {
+        fp_16_input_ptr[i] = __float2half(host_input[i]);
+    }
+
+    for (size_t i = 0; i < M * C * K * K; i++)
+    {
+        fp_16_mask_ptr[i] = __float2half(host_mask[i]);
+    }
+    
+
     wbCheck(cudaMalloc((void **)device_output_ptr, dop_sz));
     wbCheck(cudaMalloc((void **)device_input_ptr, dip_sz));
     wbCheck(cudaMalloc((void **)device_mask_ptr, dmp_sz));
 
     // wbCheck(cudaMemcpy(*device_output_ptr, host_output, dop_sz, cudaMemcpyHostToDevice));
-    // wbCheck(cudaMemcpy(*device_input_ptr, host_input, dip_sz, cudaMemcpyHostToDevice));
-    wbCheck(cudaMemcpy(*device_mask_ptr, host_mask, dmp_sz, cudaMemcpyHostToDevice));
+    wbCheck(cudaMemcpy(*device_input_ptr, fp_16_input_ptr, dip_sz, cudaMemcpyHostToDevice));
+    wbCheck(cudaMemcpy(*device_mask_ptr, fp_16_mask_ptr, dmp_sz, cudaMemcpyHostToDevice));
 
-    int streamSize = ceil(1.0f * B / NUM_STREAMS);
-
-    int W_size = ceil(1.0f*W_out/TILE_WIDTH); // number of horizontal tiles per output map
-    int H_size = ceil(1.0f*H_out/TILE_WIDTH); // number of vertical tiles per output map
-    int tileNums = H_size * W_size; // total number of tiles per map
-    dim3 DimBlock(TILE_WIDTH, TILE_WIDTH, 1); // output tile for untiled code
-    dim3 DimGrid(M, tileNums, streamSize);
-    std::cout<<"DimBlock: "<<DimBlock.x<<"x"<<DimBlock.y<<"x"<<DimBlock.z<<std::endl;
-    std::cout<<"DimGrid: "<<DimGrid.x<<"x"<<DimGrid.y<<"x"<<DimGrid.z<<std::endl;
-
-    cudaStream_t* streams = (cudaStream_t*) malloc(NUM_STREAMS * sizeof(cudaStream_t));
-
-    for (int streamNum = 0; streamNum < NUM_STREAMS; streamNum++) {
-        cudaStreamCreate(&(streams[streamNum]));
-    }
-
-    for (int streamNum = 0; streamNum < NUM_STREAMS; streamNum++) {
-        int offset = streamNum * streamSize;
-        int streamBytes = streamSize * sizeof(float);
-        //TODO: resolve this correctly, separate for input and output
-        if( (offset * sizeof(float)) + streamBytes > (B * M * H_out * W_out * sizeof(float)))
-            (void)1;
-        cudaMemcpyAsync(&(*device_input_ptr)[offset], &host_input[offset], streamBytes, cudaMemcpyHostToDevice, streams[streamNum]);
-        conv_forward_kernel<<<DimGrid, DimBlock, 0, streams[streamNum]>>>(streamNum, *device_output_ptr, *device_input_ptr, *device_mask_ptr, B, M, C, H, W, K, S);
-        cudaMemcpyAsync(&host_output[offset], &(*device_output_ptr)[offset], streamBytes, cudaMemcpyDeviceToHost, streams[streamNum]);
-    }
-
-    for (int streamNum = 0; streamNum < NUM_STREAMS; streamNum++) {
-        cudaStreamDestroy(streams[streamNum]);
-    }
-    // cudaDeviceSynchronize();
-
-    free(streams);
+    free(fp_16_input_ptr);
+    free(fp_16_mask_ptr);
 
     #undef wbCheck
 }
@@ -158,38 +143,17 @@ __host__ void GPUInterface::conv_forward_gpu(float *device_output, const float *
     // Set the kernel dimensions and call the kernel
 
     // same as inside kernel
-    // const int H_out = (H - K)/S + 1;
-    // const int W_out = (W - K)/S + 1;
-    // int W_size = ceil(1.0f*W_out/TILE_WIDTH); // number of horizontal tiles per output map
-    // int H_size = ceil(1.0f*H_out/TILE_WIDTH); // number of vertical tiles per output map
-    // int tileNums = H_size * W_size; // total number of tiles per map
-    // dim3 DimBlock(TILE_WIDTH, TILE_WIDTH, 1); // output tile for untiled code
-    // dim3 DimGrid(M, tileNums, ceil(B / NUM_STREAMS));
-    // std::cout<<"DimBlock: "<<DimBlock.x<<"x"<<DimBlock.y<<"x"<<DimBlock.z<<std::endl;
-    // std::cout<<"DimGrid: "<<DimGrid.x<<"x"<<DimGrid.y<<"x"<<DimGrid.z<<std::endl;
-
-    // cudaStream_t* streams = (cudaStream_t*) malloc(NUM_STREAMS * sizeof(cudaStream_t));
-
-    // for (int streamNum = 0; streamNum < NUM_STREAMS; streamNum++) {
-    //     cudaStreamCreate(&(streams[streamNum]));
-    // }
-
-    // size_t dop_sz = B * M * H_out * W_out * sizeof(float);
-    // size_t dip_sz = B * C * H * W * sizeof(float);
-
-    // for (int streamNum = 0; streamNum < NUM_STREAMS; streamNum++) {
-    //     int offset = streamNum * streamSize;
-    //     cudaMemcpyAsync(&device_input[offset], &host_input[offset], streamBytes, cudaMemcpyHostToDevice, stream[i]);
-    //     conv_forward_kernel<<<DimGrid, DimBlock, 0, streams[streamNum]>>>(streamNum, device_output, device_input, device_mask, B, M, C, H, W, K, S);
-    //     cudaMemcpyAsync(&host_input[offset], &device_input[offset], streamBytes, cudaMemcpyDeviceToHost, stream[i]);
-    // }
-
-    // for (int streamNum = 0; streamNum < NUM_STREAMS; streamNum++) {
-    //     cudaStreamDestroy(streams[streamNum]);
-    // }
-    // cudaDeviceSynchronize();
-
-    // free(streams);
+    const int H_out = (H - K)/S + 1;
+    const int W_out = (W - K)/S + 1;
+    int W_size = ceil(1.0f*W_out/TILE_WIDTH); // number of horizontal tiles per output map
+    int H_size = ceil(1.0f*H_out/TILE_WIDTH); // number of vertical tiles per output map
+    int tileNums = H_size * W_size; // total number of tiles per map
+    dim3 DimBlock(TILE_WIDTH, TILE_WIDTH, 1); // output tile for untiled code
+    dim3 DimGrid(M, tileNums, B);
+    std::cout<<"DimBlock: "<<DimBlock.x<<"x"<<DimBlock.y<<"x"<<DimBlock.z<<std::endl;
+    std::cout<<"DimGrid: "<<DimGrid.x<<"x"<<DimGrid.y<<"x"<<DimGrid.z<<std::endl;
+    conv_forward_kernel<<<DimGrid, DimBlock>>>((__half*) device_output, (__half*) device_input, (__half*) device_mask, B, M, C, H, W, K, S);
+    cudaDeviceSynchronize();
 }
 
 
@@ -208,16 +172,24 @@ __host__ void GPUInterface::conv_forward_gpu_epilog(float *host_output, float *d
 
     const int H_out = (H - K)/S + 1;
     const int W_out = (W - K)/S + 1;
-    size_t dop_sz = B * M * H_out * W_out * sizeof(float);
+    size_t dop_sz = B * M * H_out * W_out * sizeof(__half);
+
+    __half* fp_16_output_ptr = (__half*) malloc(dop_sz);
 
     // Copy the output back to host
-    // wbCheck(cudaMemcpy(host_output, device_output, dop_sz, cudaMemcpyDeviceToHost));
+    wbCheck(cudaMemcpy(fp_16_output_ptr, device_output, dop_sz, cudaMemcpyDeviceToHost));
 
+    for (size_t i = 0; i < B * M * H_out * W_out; i++)
+    {
+        host_output[i] = __half2float(fp_16_output_ptr[i]);
+    }
    
     // Free device memory
     wbCheck(cudaFree(device_input));
     wbCheck(cudaFree(device_output));
     wbCheck(cudaFree(device_mask));
+
+    free(fp_16_output_ptr);
 
     #undef wbCheck
 }
